8c4cf4c1c672d39f8f59cc7c4fd11cc59dfc84f1
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-21 21:00:55 +0500

    initial commit

---
+++ src/main/java/dev/muhammad/hibernatejpa/Application.java
@@ -0,0 +1,20 @@
+package dev.muhammad.hibernatejpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return runner -> System.out.println("Hello, I am running in the command line");
+    }
+
+}
---
+++ src/test/java/dev/muhammad/hibernatejpa/RevisionHibernateJpaSection3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muhammad.hibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionHibernateJpaSection3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
